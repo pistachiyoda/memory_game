ad7e5ae0af65ebabcbaaacc56213aa270936f27b
mai <fujisawamai21@gmail.com>
2020-03-09 00:22:36 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,153 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Memory Game</h1>
+    <h2>Parrot ver.</h2>
+    <div class="container">
+      <div class="row">
+        <div
+          v-for="(parrot, index) in shuffled_parrot_list"
+          :key="index"
+          class="col-3"
+        >
+          <Parrot :parrot="parrot"></Parrot>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Parrot from "./components/Parrot.vue";
+import _ from "lodash";
+const parrot_master = [
+  "/images/60fpsparrot.gif",
+  "/images/angelparrot.gif",
+  "/images/angryparrot.gif",
+  "/images/aussiecongaparrot.gif",
+  "/images/aussieparrot.gif",
+  "/images/aussiereversecongaparrot.gif",
+  "/images/backwardsparrot.gif",
+  "/images/beerparrot.gif",
+  "/images/beretparrot.gif",
+  "/images/bikerparrot.gif",
+  "/images/birthdaypartyparrot.gif",
+  "/images/bluntparrot.gif",
+  "/images/bobaparrot.gif",
+  "/images/bootlegparrot.gif",
+  "/images/boredparrot.gif",
+  "/images/bouncingparrot.gif",
+  "/images/brazilianfanparrot.gif",
+  "/images/brazilianplayerparrot.gif",
+  "/images/bunnyparrot.gif",
+  "/images/calvinist_parrot.gif",
+  "/images/ceilingparrot.gif",
+  "/images/chicoparrot.gif",
+  "/images/christmasparrot.gif",
+  "/images/confusedparrot.gif",
+  "/images/congaparrot.gif",
+  "/images/congapartyparrot.gif",
+  "/images/copparrot.gif",
+  "/images/dadparrot.gif",
+  "/images/darkmodeparrot.gif",
+  "/images/dealwithitnowparrot.gif",
+  "/images/dealwithitparrot.gif",
+  "/images/discoparrot.gif",
+  "/images/donutparrot.gif",
+  "/images/evilparrot.gif",
+  "/images/fastparrot.gif",
+  "/images/flowerparrot.gif",
+  "/images/flyingmoneyparrot.gif",
+  "/images/footballparrot.gif",
+  "/images/frenchparrot.gif",
+  "/images/gentlemanparrot.gif",
+  "/images/githubparrot.gif",
+  "/images/gothparrot.gif",
+  "/images/grouchoparrot.gif",
+  "/images/hardhatparrot.gif",
+  "/images/harpoparrot.gif",
+  "/images/headingparrot.gif",
+  "/images/headsetparrot.gif",
+  "/images/horizontalparrot.gif",
+  "/images/illuminatiparrot.gif",
+  "/images/inverseparrot.gif",
+  "/images/invisibleparrot.gif",
+  "/images/jediparrot.gif",
+  "/images/laptop_parrot.gif",
+  "/images/levitationparrot.gif",
+  "/images/maracasparrot.gif",
+  "/images/mardigrasparrot.gif",
+  "/images/marshmallowparrot.gif",
+  "/images/michaeljacksonparrot.gif",
+  "/images/middleparrot.gif",
+  "/images/moonwalkingparrot.gif",
+  "/images/mustacheparrot.gif",
+  "/images/norwegianblueparrot.gif",
+  "/images/opensourceparrot.gif",
+  "/images/originalparrot.gif",
+  "/images/parrot.gif",
+  "/images/parrotnotfound.gif",
+  "/images/partyparrot.gif",
+  "/images/phparrot.gif",
+  "/images/picassoparrot.gif",
+  "/images/pingpongparrot.gif",
+  "/images/pirateparrot.gif",
+  "/images/popcornparrot.gif",
+  "/images/portalblueparrot.gif",
+  "/images/portalorangeparrot.gif",
+  "/images/pumpkinparrot.gif",
+  "/images/redenvelopeparrot.gif",
+  "/images/redhatparrot.gif",
+  "/images/reversecongaparrot.gif",
+  "/images/reverseparrot.gif",
+  "/images/reverseportalblueparrot.gif",
+  "/images/reverseportalorangeparrot.gif",
+  "/images/revolutionparrot.gif",
+  "/images/sadparrot.gif",
+  "/images/sassyparrot.gif",
+  "/images/scienceparrot.gif",
+  "/images/sherlockholmesparrot.gif",
+  "/images/shortparrot.gif",
+  "/images/shuffleparrot.gif",
+  "/images/sintparrot.gif",
+  "/images/sithparrot.gif",
+  "/images/sleepingparrot.gif",
+  "/images/slowparrot.gif",
+  "/images/spinningparrot.gif",
+  "/images/spyparrot.gif",
+  "/images/stableparrot.gif",
+  "/images/sushiparrot.gif",
+  "/images/tennisparrot.gif",
+  "/images/thumbsupparrot.gif",
+  "/images/tiedyeparrot.gif",
+  "/images/transparront.gif",
+  "/images/twinsparrot.gif",
+  "/images/ultrafastparrot.gif",
+  "/images/verticalparrot.gif",
+  "/images/vikingparrot.gif",
+  "/images/wendyparrot.gif",
+  "/images/whitewalkerparrot.gif",
+  "/images/yosemitesamparrot.gif"
+];
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Parrot
+  },
+  data: function() {
+    return {
+      parrot_list: [],
+      shuffled_parrot_list: []
+    };
+  },
+  created: function() {
+    const random_parrots = _.sampleSize(parrot_master, 8);
+    this.parrot_list = random_parrots;
+    this.shuffled_parrot_list = _.shuffle([
+      ...random_parrots,
+      ...random_parrots
+    ]);
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Parrot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper" @click="show_parrot">
+    <img v-if="!this.is_clicked" />
+    <img v-else :src="parrot" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    parrot: String
+  },
+  data: function() {
+    return {
+      is_clicked: false
+    };
+  },
+  methods: {
+    show_parrot: function() {
+      const self = this;
+      this.is_clicked = true;
+      window.setTimeout(function() {
+        self.is_clicked = false;
+      }, 2000);
+    }
+  }
+};
+</script>
+<style>
+.wrapper {
+  height: 128px;
+  width: 128px;
+  border: medium solid black;
+}
+</style>
